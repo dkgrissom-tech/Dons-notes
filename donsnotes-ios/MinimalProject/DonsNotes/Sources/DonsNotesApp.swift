d899f1c0c76e9d19073fdb4b50aa6a9d24a4cf20
Don <don@donsnotes.com>
2026-06-05 03:38:21 +0000

    Simplify app - remove AppDelegate from minimal project

--- donsnotes-ios/MinimalProject/DonsNotes/Sources/DonsNotesApp.swift
+++ donsnotes-ios/MinimalProject/DonsNotes/Sources/DonsNotesApp.swift
@@ -3,7 +3,7 @@ import SwiftUI
 @main
 struct DonsNotesApp: App {
     @StateObject var apiService = MockAPIService()
-    
+
     var body: some Scene {
         WindowGroup {
             MeetingListView(apiService: apiService)
